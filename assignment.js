b93a09651d0a12b8dbf025d662c8e165b4b7cf18
wajhi rizvi <smart.wajhee96@gmail.com>
2026-02-21 04:23:32 +0500

    Js.Assignment

---
+++ assignment.js
@@ -0,0 +1,71 @@
+chapter = 1
+
+// Que 1
+// alert("Wellcome javascript!")
+
+// Que 2
+// alert("lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue")
+
+// Que 3
+// alert("Wellcome to java! \n Happy Codding")
+
+// Que 4
+// alert("Wajji here")
+// alert("Assignment Done")
+
+
+Chapter = 2
+
+// Que 1
+// var firstName = "Wajhi";
+
+// Que 2
+// var lastName = "Rizvi";
+
+// Que 3
+// var fullName = firstName + " " + lastName
+// console.log(fullName)
+
+// Que 4
+// alert(fullName);
+
+// Que 5
+// studentName = "Wajhi"
+// studentAge = "21"
+// studentCourse = "Webdevelopment"
+// alert("Name: " + studentName);
+// alert("Age: " + studentAge);
+// alert("Course: " + studentCourse);
+
+// Que 6
+// var pizza = "BURGER\nBURGE\nBURG\nBUR\nBU\nB";
+// alert(pizza);
+
+// Que 7
+//  var email = "smart.wajhee96@gmail.com";
+//  alert("My email address is " + email);  
+
+// Que 8
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book " + book);
+
+Chapter = 3
+
+// Que 1
+// var age = 21;
+// alert("I am " + age + " years old");
+
+// Que 2   
+// var visitCount = 14;
+// alert("You have visited this site " + visitCount + " times");
+
+// Que 3   
+// var birthYear = 2004;
+// document.write("My birth year is " + birthYear + "<br>");
+// document.write("Data type of my declared variable is number");
+
+// Que 4
+//  var visitorName = "Wajji Rizvi";
+//  var productTitle = "Drop-Shoulders";
+//  var quantity = 5;
+//  document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on ABC Clothing store");
